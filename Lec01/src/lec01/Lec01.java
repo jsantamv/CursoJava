8c93a1a688c99cb834ee1dd840ad3b1c05999774
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-04-02 22:06:18 -0600

    actualiciones varias

--- Lec01/src/lec01/Lec01.java
+++ Lec01/src/lec01/Lec01.java
@@ -1,18 +1,40 @@
 package lec01;
 
 import javax.swing.JOptionPane;
-import prueba1.Pregunta1;
+import prueba1.Articulos;
+
 
 
 public class Lec01 {
 
     public static void main(String[] args) {
         
-        Pregunta1 miPreg1 = new Pregunta1();
-        String dato1 = miPreg1.Pregunta();
+        Articulos[] misArticulos = new Articulos[1];
+        String eti = "NO";
+        for (int i = 0; i < misArticulos.length; i++) {
+            
+            int contador = i+1;
+            Articulos miArticulo = new Articulos();            
+            miArticulo.Nombre = (String)JOptionPane.showInputDialog(null,"Articulo No: "+ contador);
+            miArticulo.Cantidad =  Integer.parseInt(JOptionPane.showInputDialog(null,"Cantidad: "+ contador));
+            miArticulo.Precio = Double.parseDouble(JOptionPane.showInputDialog(null,"Precio: "+ contador));
+            
+            String etiqueta = (String)(JOptionPane.showInputDialog(null,"Etiquita Roca (S/N)"+ contador));
+            
+            if (etiqueta.toUpperCase().equals("S"))
+            {
+                miArticulo.EtiquetaRoja = true;  
+                eti = "SI";
+            }
+             
+            misArticulos[i] = miArticulo;
+        }
         
-        System.out.println(dato1);
-
+        for (int i = 0; i < misArticulos.length; i++) {
+            
+            System.out.println("Articulo:"+misArticulos[i].Nombre +" Cantidad: "+misArticulos[i].Cantidad+" Precio: "+misArticulos[i].Precio +" Etiqueta Roja "+ eti);
+        }
+        
     }
     
     
---
+++ Lec01/src/prueba1/Articulos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+/**
+ *
+ * @author Juan Carlos
+ */
+public class Articulos {
+    
+    public String Nombre;
+    public Double Precio;
+    public Integer Cantidad;
+    public boolean EtiquetaRoja = false;
+    
+}
