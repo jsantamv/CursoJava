0ec1b4ec485c4784d58a8ee3e1406dc15d18ecad
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-08-25 21:06:59 -0600

    SE TERMINA LAS VALIDACIONES

--- src/practicafinal/ClubLeon.java
+++ src/practicafinal/ClubLeon.java
@@ -8,7 +8,8 @@ package practicafinal;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * Esta clase me lleva el control de los club 
+ * que se van creando.
  * @author Susana Santamaria
  */
 public class ClubLeon {
@@ -17,39 +18,13 @@ public class ClubLeon {
     float Recolecta;
 
     /**
-     * Constructor inicaliza vacio
+     * Constructor inicaliza 
+     * las propiedades
      */
     public ClubLeon() {
 
         this.Recolecta = 0;
         this.Nombre = "";
-    }
-
-    /**
-     * Constructor sobrecargado para inicializar los datos que se le pasan por
-     * paramtro
-     *
-     * @param recolecta
-     * @param nombre
-     */
-    public ClubLeon(int recolecta, String nombre) {
-        this.Recolecta = recolecta;
-        this.Nombre = nombre;
-    }
-
-    public ClubLeon[] SolicitarClub() {
-        ClubLeon[] miclub = new ClubLeon[5];
-        
-        for (ClubLeon miclub1 : miclub) {
-            miclub1.Nombre = asignarDato();
-        }
-        
-        return miclub;
-    }
-    
-    private String asignarDato() {
-        String respuesta = JOptionPane.showInputDialog("Digite el nombre del Club");
-        return respuesta;
     }
 
 }
--- src/practicafinal/PracticaFinal.java
+++ src/practicafinal/PracticaFinal.java
@@ -5,8 +5,9 @@
  */
 package practicafinal;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
-import java.util.Scanner;
 
 /**
  *
@@ -14,62 +15,103 @@ import java.util.Scanner;
  */
 public class PracticaFinal {
 
-    public ClubLeon unClub[] = new ClubLeon[5];
-//    private int num = 0;
+    static Integer MAX_CLUB = 2;
+    static List<ClubLeon> clubList = new ArrayList();
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
-        ClubLeon club = new ClubLeon();
-        
-        unClub = club.SolicitarClub();
-        
+
+        /*PASO 1 CREAMOS LOS CLUBS */
+        for (int i = 0; i < MAX_CLUB; i++) {
+            ClubLeon miClub = new ClubLeon();
+            miClub.Nombre = asignarDato();
+            clubList.add(miClub);
+        }
+
+        //PASO 2 RECOLECTAMOS LOS DINEROS
+        recolectarDinero(clubList);
+
+        //PASO 3 IMPRIMIMOS LOS RESULTADOS
+        imprimirResultados(clubList);
     }
 
-    /**
-     * Metodo para Guardar el Club
-     *
-     * @param nombre del club
-     * @return un ClubLeon
-     */
-    public ClubLeon guardarClub(String nombre) {
-
-        ClubLeon cl = new ClubLeon();
-        cl.Nombre = nombre;
-        return cl;
-
-    }
-
-    public static String asignarDato() {
-        String respuesta = JOptionPane.showInputDialog(null, "Digite el nombre del Club");
+    private static String asignarDato() {
+        String respuesta = JOptionPane.showInputDialog("Digite el nombre del Club");
         return respuesta;
     }
 
-    public static void mostarDato(String dato) {
-        JOptionPane.showMessageDialog(null, dato);
+    private static String selecionarClub(List<ClubLeon> _club) {
+        String[] club = new String[_club.size()];
+        for (int i = 0; i < _club.size(); i++) {
+            club[i] = _club.get(i).Nombre;
+        }
+
+        String resp = (String) JOptionPane.showInputDialog(null, "Selecione el Club para Recolecta", "Club de Leon", JOptionPane.DEFAULT_OPTION, null, club, club[0]);
+        return resp;
     }
 
     /**
-     * Metodo ejemplo no se usa.
+     * RECOLECTAMOS LOS DINEROS
+     * @param _club 
      */
-    public void auxiliares() {
-        String respuesta = JOptionPane.showInputDialog(null, "Escribe tu nombre");
+    private static void recolectarDinero(List<ClubLeon> _club) {
+        ClubLeon nuevoClub = new ClubLeon();
+        String miclub = selecionarClub(_club);
+        Integer posClub = 0;
+        String salida = "X";
+        String recuado = "";
+        int resp = 1;  //para salir
 
-        String[] carreras = {
-            "Ingeniería en sistemas computacionales",
-            "Ingeniería industrial",
-            "Ingeniería en mecatrónica",
-            "Ingeniería en informatica",
-            "Ingeniería petroquímica"
-        };
-        //MyIcon icon = new MyIcon();
-        String resp = (String) JOptionPane.showInputDialog(null, "Seleccione una carrera a cursar", "Carrera", JOptionPane.DEFAULT_OPTION, null, carreras, carreras[0]);
+        do { //PARA SELECIONAR OTRA VEZ EL CLUB
 
-        int resps = JOptionPane.showConfirmDialog(null, "¿Está seguro?");
+            for (int i = 0; i < _club.size(); i++) {
+                if (_club.get(i).Nombre.equals(miclub)) {
+                    nuevoClub.Nombre = _club.get(i).Nombre;
+                    nuevoClub.Recolecta = _club.get(i).Recolecta;
+                    posClub = i;
+                }
+            }
 
-        int respa = JOptionPane.showConfirmDialog(null, "¿Esta seguro?", "Alerta!", JOptionPane.YES_NO_OPTION);
+            do {//para digitar los montos por el club
+                recuado = JOptionPane.showInputDialog("Digite el Monto para recuado del Club de Leon: [" + miclub + "] X: Salir");
+
+                if (isNumeric(recuado)) {
+                    nuevoClub.Recolecta = nuevoClub.Recolecta + Float.parseFloat(recuado);
+                    clubList.set(posClub, nuevoClub);
+                } else {
+                    if (!recuado.equals("X")) {
+                        JOptionPane.showMessageDialog(null, " NO es dato valido: " + recuado);
+                    }
+                }
+            } while (!salida.equals(recuado));
+
+            resp = JOptionPane.showConfirmDialog(null, "Selecionar Otro Club", "Alerta!", JOptionPane.YES_NO_OPTION);
+
+            if (resp == 0) {
+                miclub = selecionarClub(_club);
+            }
+
+        } while (resp == 0);
+    }
+
+    //AUXILIAR PARA VALIDAR SI ES NUMERICO UN DATO
+    private static boolean isNumeric(String cadena) {
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+
+    private static void imprimirResultados(List<ClubLeon> club) {
+
+        String dato = "";
+
+        for (int i = 0; i < club.size(); i++) {
+            dato = club.get(i).Nombre + " Recolecta: " + clubList.get(i).Recolecta;
+            //JOptionPane.showMessageDialog(null, dato);
+            System.out.println(dato);
+        }
     }
 
 }
